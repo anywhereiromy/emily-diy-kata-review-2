8603168dd15f6784c2a5ab0c2b6d47ea578f87da
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2021-12-03 16:14:30 +0000

    completed humanCatDogYears

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,4 +1,19 @@
 const humanCatDogYears = number => {
+  if (number === 1) {
+    catYear = 15;
+    dogYear - 15
+  }
+
+  if (number === 2) {
+    catYear = 24;
+    dogYear = 24;
+  }
+
+  if (number > 3) {
+    catYear = 24 + (number - 2) * 4;
+    dogYear = 24 + (number - 2) * 5;
+  }
+  return [number, catYear, dogYear];
 };
 
 module.exports = humanCatDogYears;

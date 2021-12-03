6b8b156b06864f69ea63ecfa688434a2775f2d7f
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2021-12-03 16:57:29 +0000

    added 2nd test to humanCatDogYears test and passed test for reachDestination

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -3,5 +3,6 @@ const { humanCatDogYears } = require("../src");
 describe("humanCatDogYears", () => {
   test("returns array of human, cat and dog years when passed human years", () => {
     expect(humanCatDogYears(10)).toStrictEqual([10, 56, 64]);
+    expect(humanCatDogYears(6)).toStrictEqual([6, 40, 44]);
   });
 });

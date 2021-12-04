5c884ce8226e927e977e18dee80347a9b08be252
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2021-12-04 10:55:37 +0000

    passed employee role test

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,6 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+  const obj = employees.find(element => element.name === employeeName);
+  return obj.role;
+};
 
 module.exports = getEmployerRole;

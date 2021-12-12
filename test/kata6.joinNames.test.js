1b7cd92b53a73b618591a77c09a120b3a661c0d9
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2021-12-12 14:53:38 +0000

    tidied up the joinNames test

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,15 +1,17 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
+  const bart = { name: "Bart" };
+  const lisa = { name: "Lisa" };
+  const maggie = { name: "Maggie" };
+  const alison = { name: "Alison" };
+  const emily = { name: "Emily" };
+  const jessie = { name: "Jessie" };
   test("returns string of names, seperated by commas and an ampersand", () => {
-    expect(
-      joinNames([{ name: "Bart" }, { name: "Lisa" }, { name: "Maggie" }])
-    ).toEqual("Bart, Lisa & Maggie");
-    expect(
-      joinNames([{ name: "Alison" }, { name: "Emily" }, { name: "Jessie" }])
-    ).toEqual("Alison, Emily & Jessie");
-    expect(joinNames([{ name: "Alison" }, { name: "Jessie" }])).toEqual(
-      "Alison & Jessie"
+    expect(joinNames([bart, lisa, maggie])).toEqual("Bart, Lisa & Maggie");
+    expect(joinNames([alison, emily, jessie])).toEqual(
+      "Alison, Emily & Jessie"
     );
+    expect(joinNames([alison, jessie])).toEqual("Alison & Jessie");
   });
 });

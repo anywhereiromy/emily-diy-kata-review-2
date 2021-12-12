ee7ade9fcdeaab71e575b255f0e94823eefbab89
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2021-12-12 14:45:28 +0000

    changed humanCatDogYears solution

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,15 +1,17 @@
 const humanCatDogYears = number => {
+  let catYear = number;
+  let dogYear = number;
   if (number === 1) {
-    catYear = 15;
-    dogYear - 15
+    catYear = number + 14;
+    dogYear = number + 14;
   }
 
   if (number === 2) {
-    catYear = 24;
-    dogYear = 24;
+    catYear = 15 + 9;
+    dogYear = 15 + 9;
   }
 
-  if (number > 3) {
+  if (number >= 3) {
     catYear = 24 + (number - 2) * 4;
     dogYear = 24 + (number - 2) * 5;
   }

19d287c18ed673cf5da56daccb9a28193a3dff8e
“EmilyRoseCotter” <emilyrose_95@hotmail.co.uk>
2021-12-12 14:56:56 +0000

    Added a test case argument to reachDestination where the second parameter isn't 10

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -4,5 +4,6 @@ describe("reachDestination", () => {
   test("returns string with estimated time of arrival", () => {
     expect(reachDestination(44, 10)).toEqual("I should be there in 4.5 hours.");
     expect(reachDestination(20, 10)).toEqual("I should be there in 2 hours.");
+    expect(reachDestination(32, 5)).toEqual("I should be there in 6.5 hours.");
   });
 });
